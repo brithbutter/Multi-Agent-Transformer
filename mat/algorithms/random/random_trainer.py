88b036e3014040aea3f8c47637c642c244b0f8fc
brithbutter_gpu2 <brithbutter@sina.com>
2023-10-12 12:48:36 -0300

    Modify for random

---
+++ mat/algorithms/random/random_trainer.py
@@ -0,0 +1,36 @@
+
+class RandomTrainer:
+    def __init__(self,
+                 args,
+                 policy,
+                 num_agents,
+                 device=torch.device("cpu")):
+
+        self.device = device
+        self.tpdv = dict(dtype=torch.float32, device=device)
+        self.policy = policy
+        self.num_agents = num_agents
+
+        self.clip_param = args.clip_param
+        self.ppo_epoch = args.ppo_epoch
+        self.num_mini_batch = args.num_mini_batch
+        self.data_chunk_length = args.data_chunk_length
+        self.value_loss_coef = args.value_loss_coef
+        self.entropy_coef = args.entropy_coef
+        self.max_grad_norm = args.max_grad_norm       
+        self.huber_delta = args.huber_delta
+
+        self._use_recurrent_policy = args.use_recurrent_policy
+        self._use_naive_recurrent = args.use_naive_recurrent_policy
+        self._use_max_grad_norm = args.use_max_grad_norm
+        self._use_clipped_value_loss = args.use_clipped_value_loss
+        self._use_huber_loss = args.use_huber_loss
+        self._use_valuenorm = args.use_valuenorm
+        self._use_value_active_masks = args.use_value_active_masks
+        self._use_policy_active_masks = args.use_policy_active_masks
+        self.dec_actor = args.dec_actor
+        
+        if self._use_valuenorm:
+            self.value_normalizer = ValueNorm(1, device=self.device)
+        else:
+            self.value_normalizer = None
