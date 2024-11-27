435458d1ecbfcbe7550e014fd8b0039ce6885eb9
brithbutter_gpu2 <brithbutter@sina.com>
2024-11-27 12:19:22 -0400

    update config for momat

--- mat/config.py
+++ mat/config.py
@@ -158,7 +158,7 @@ def get_config():
 
     # prepare parameters
     parser.add_argument("--algorithm_name", type=str,
-                        default='mat', choices=["mat", "mat_dec", "mat_encoder", "mat_decoder", "mat_gru","happo","rmappo","random","hatrpo","ppo","ippo"])
+                        default='mat', choices=["mat","momat", "mat_dec", "mat_encoder", "mat_decoder", "mat_gru","happo","rmappo","random","hatrpo","ppo","ippo"])
 
     parser.add_argument("--experiment_name", type=str, default="check", help="an identifier to distinguish different experiment.")
     parser.add_argument("--seed", type=int, default=1, help="Random seed for numpy/torch")
@@ -173,6 +173,8 @@ def get_config():
                         help="Number of parallel envs for evaluating rollouts")
     parser.add_argument("--n_render_rollout_threads", type=int, default=1,
                         help="Number of parallel envs for rendering rollouts")
+    parser.add_argument("--n_objective", type=int, default=1,
+                        help="Number of objectives to be optimized")
     parser.add_argument("--num_env_steps", type=int, default=7e5,
                         help='Number of environment steps to train (default: 10e6)')
     parser.add_argument("--user_name", type=str, default='xxx',help="[for wandb usage], to specify user's name for simply collecting training data.")
